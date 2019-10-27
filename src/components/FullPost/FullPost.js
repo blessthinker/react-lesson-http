03507f2db2be6429b5e95febeb86cc91407bdaf2
W Rumaizi <blessthinker@gmail.com>
2019-10-28 01:31:43 +0800

    fetch content based selected post and display selected content title.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./FullPost.css";
 
 class FullPost extends Component {
+  state = {
+    loadedPost: null
+  };
+  componentDidUpdate() {
+    //check if id is not null
+    if (this.props.id) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id != this.props.id)
+      ) {
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .then(response => {
+            this.setState({ loadedPost: response.data });
+          });
+      }
+    }
+  }
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Post is loading</p>;
+    }
+
+    if (this.props.id && this.state.loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
+          <h1>{this.state.loadedPost.title}</h1>
           <p>Content</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
